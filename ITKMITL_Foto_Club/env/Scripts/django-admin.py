16f4d1baef8754b780f29e552757a6330414eddb
Teerasak61070094 <42912705+Teerasak61070094@users.noreply.github.com>
2020-03-25 20:39:53 +0700

    Start

---
+++ ITKMITL_Foto_Club/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\61070094\Desktop\KMITL\P_DB_WEBPRO_ISAD\WEBPRO\ITKMITL_Foto_Club\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
