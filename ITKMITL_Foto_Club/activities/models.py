c2c0c8e30acaa886455b8e9d0ffb94b524b2ddcd
Teerasak61070094 <42912705+Teerasak61070094@users.noreply.github.com>
2020-04-14 21:36:56 +0700

    model ++

--- ITKMITL_Foto_Club/activities/models.py
+++ ITKMITL_Foto_Club/activities/models.py
@@ -5,8 +5,28 @@ class Activities(models.Model):
     activity_title = models.CharField(max_length=50)
     picture_path = models.URLField(max_length = 500,null = True) 
     location = models.TextField()
-    staff = models.ForeignKey(Staff, on_delete=models.CASCADE)
 
 class Staff(models.Model):
     staff_id = models.ForeignKey(User, on_delete=models.CASCADE)
     activity_id = models.ForeignKey(Activities, on_delete=models.CASCADE)
+    
+class Album(models.Model):
+    activity_id = models.ForeignKey(Activities, on_delete=models.CASCADE)
+    album_name = activity_title = models.CharField(max_length=50)
+
+class Picture(models.Model):
+    album_id = models.ForeignKey(Album, on_delete=models.CASCADE)
+    album_name = activity_title = models.CharField(max_length=50)
+    picture_path = models.URLField(max_length = 500,null = True)
+
+class Contact(models.Model):
+    activity_id = models.ForeignKey(Activities, on_delete=models.CASCADE)
+    contact_person = models.CharField(max_length=50)
+    contact_number = models.CharField(max_length=10)
+
+class Datetime(models.Model):
+    start_time = DateTimeField(auto_now=True)
+    finish_time = DateTimeField(auto_now=True)
+    activity_id = models.ForeignKey(Activities, on_delete=models.CASCADE)
+
+    
