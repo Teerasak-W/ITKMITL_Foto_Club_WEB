b045b0d61ba1a6253766739d494219b107ddb81d
Teerasak <61070094@kmitl.ac.th>
2020-04-15 21:17:31 +0700

    Update models+

--- ITKMITL_Foto_Club/account/models.py
+++ ITKMITL_Foto_Club/account/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+class Equipment(models.Model):
+    type_for = (('Ca', 'camera'),('Le', 'lens'),('Si', 'light'),('Ot', 'other'))
+    equipment_type = models.CharField(max_length=2, choices=type_for)
+    equipment_detail = models.TextField()
+    equipment_title = models.CharField(max_length=50)
+
 class Request(models.Model):
     request_title = models.CharField(max_length=50)
     location = models.TextField()
@@ -8,13 +14,16 @@ class Request(models.Model):
     detail = models.TextField()
     request_status = models.BooleanField(default=False)
 
-class Equipment(models.Model):
-    type_for = (('Ca', 'camera'),('Le', 'lens'),('Si', 'light'),('Ot', 'other'))
-    equipment_type = models.CharField(max_length=2, choices=type_for)
-    equipment_detail = models.TextField()
-    equipment_title = models.CharField(max_length=50)
+class activity_contact(models.Model):
+    name = models.CharField(max_length=50)
+    number = models.CharField(max_length=10)
 
-class Date_time(models.Model):
+class Request_Date_time(models.Model):
     start_time = models.DateTimeField(auto_now=True)
     finish_time = models.DateTimeField(auto_now=True)
-    activity_id = models.ForeignKey(Activities, on_delete=models.CASCADE)
+
+class Audience_suggestion(models.Model):
+    detail = models.TextField()
+
+class Member_suggestion(models.Model):
+    detail = models.TextField()
