e630e2c1d72c9f8a550987c6bbe488a6370f251d
Teerasak61070094 <42912705+Teerasak61070094@users.noreply.github.com>
2020-04-19 21:06:52 +0700

    Update views

--- ITKMITL_Foto_Club/account/views.py
+++ ITKMITL_Foto_Club/account/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.generic import TemplateView
+from django.core.files.storage import FileSystemStorage
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
 
 # Create your views here.
+def my_login(request):
+    context = {}
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('index')
+        else:
+            context['username'] = username
+            context['password'] = password
+            context['error'] = 'Wrong username or password!'
+
+    return render(request, template_name='login.html', context=context)
+
+def my_logout(request):
+    logout(request)
+    return redirect('login')
+
