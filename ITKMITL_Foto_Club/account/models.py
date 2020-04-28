35610d625b6dd6c564005b4c16ff8122c87cc30c
Teerasak <61070094@kmitl.ac.th>
2020-04-28 16:11:30 +0700

    D bug and update Ajax

--- ITKMITL_Foto_Club/account/models.py
+++ ITKMITL_Foto_Club/account/models.py
@@ -5,6 +5,7 @@ class User_Account(models.Model):
     user_id = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,)
     student_id = models.CharField(max_length=8)
     picture_path = models.FileField(upload_to='user')
+    pass_code = models.CharField(max_length=5)
     audience = models.BooleanField(default=True)
     member = models.BooleanField(default=False)
     admin = models.BooleanField(default=False)
